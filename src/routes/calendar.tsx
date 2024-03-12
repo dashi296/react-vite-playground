610cfec34a065706141aab3cc1ff4186aeb5c77c
shun okada <falcon0296@gmail.com>
2024-03-12 16:00:00 +0900

    [fix] 不要な記述を削除

--- src/routes/calendar.tsx
+++ src/routes/calendar.tsx
@@ -1,23 +1,10 @@
-import { createFileRoute /*useSearch*/ } from "@tanstack/react-router";
-// import z from "zod";
-
-/*const searchSchema = z.object({
-  yearMonth: z
-    .string()
-    .regex(/^\d{4}-\d{2}$/)
-    .optional(),
-});*/
+import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/calendar")({
   component: CalendarPage,
-  // validateSearch: searchSchema,
 });
 
 export default function CalendarPage() {
-  /*const search = useSearch({
-    from: Route.fullPath,
-  })*/
-
   return (
     <div className="p-2">
       <h3>Calendar</h3>
